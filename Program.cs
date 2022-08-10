394a6c65af0f509aa1c1c8755da9663a283fa7ea
Vadim Pudov <pvb@rambler.ru>
2022-08-10 20:18:41 +0700

    task13

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+
+var num = 0;
+var str = "";
+
+while(true) {
+Console.WriteLine("Введите любое число: ");
+str = Console.ReadLine();
+if(int.TryParse(str, out int number)) {
+    num = number;
+    if(str.Length < 3) {
+        Console.WriteLine("Третьей цифры у числа " + num + " нет!");
+        break;
+    }
+    else {
+        Console.WriteLine("Третья цифра числа " + num + ": " + str[2]);
+        break;
+    }
+}
+else Console.WriteLine("Некорректно введено число!");
+}
